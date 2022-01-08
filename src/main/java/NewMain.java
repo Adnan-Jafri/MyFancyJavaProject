71b3e764c29300ff28a1ced8b8eee7fb8c9e449d
Tanim-Kabir <tkabir172101@bscse.uiu.ac.bd>
2022-01-08 16:16:43 +0600

    Singleton Pattern Implemented!

--- src/main/java/NewMain.java
+++ src/main/java/NewMain.java
@@ -1,9 +1,34 @@
-
-public class NewMain {
-
+class InstanceClass {
+    private static InstanceClass instance = null;
     
+    private InstanceClass(String name, String id) {
+        User user = new User(name, id);
+    }
+    
+    public static InstanceClass getInstanceClass(String name, String id) {
+        if(instance == null) {
+            instance = new InstanceClass(name, id);
+        }
+        return instance;
+    }
+}
+
+class User {
+    String name;
+    String id;
+    
+    public User(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
+
+
+public class Main {
     public static void main(String[] args) {
-        String name = "Adnan Jafri";
-        String id = "011172184";
+        String name = "Tanim Kabir";
+       String id = "011172101";
+       
+       InstanceClass student = InstanceClass.getInstanceClass(name, id);
     }
 }
