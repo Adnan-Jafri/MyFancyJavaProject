fcace3454c75183ff66827ee960f6a8026c361b6
Adnan-Jafri <aanik172184@bscse.uiu.ac.bd>
2022-01-09 16:38:11 +0600

    Changed Slightly on Main Branch

--- src/main/java/NewMain.java
+++ src/main/java/NewMain.java
@@ -5,5 +5,7 @@ public class NewMain {
     public static void main(String[] args) {
         String name = "Adnan Jafri";
         String id = "011172184";
+
+        System.out.print("Name: "+name+"\nID: "+id);
     }
 }
